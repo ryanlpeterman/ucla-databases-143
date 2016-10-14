01fb40bc95ba1be8fde82a82983ed4b79aa745bb
Ryan Peterman <RyanLPeterman@gmail.com>
2016-10-13 20:50:44 -0700

    finish part 1 of Project 1B

---
+++ project1b/queries.sql
@@ -0,0 +1,9 @@
+/* All Actors in movie "Die Another Day" */
+SELECT first, last
+FROM Movie m, MovieActor ma, Actor a
+WHERE m.id=ma.mid AND m.title="Die Another Day" AND ma.aid=a.id;
+
+/* Counts number of actors in more than one movie */
+SELECT COUNT(*) FROM (SELECT DISTINCT ma1.aid
+FROM MovieActor ma1, MovieActor ma2
+WHERE ma1.mid!=ma2.mid AND ma1.aid=ma2.aid) AS MULT_MOVE_ACTORS;
