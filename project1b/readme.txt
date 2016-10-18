e16ee71fa6c78f80c866ab3502ea08efc28535ca
Ryan Peterman <RyanLPeterman@gmail.com>
2016-10-17 22:54:45 -0700

    project1b completely done

---
+++ project1b/readme.txt
@@ -0,0 +1,14 @@
+Project 1B CS143
+Names: Max Chern | Ryan Peterman
+UID:   304290221 | 704269982
+
+Overall Method:
+    1. Used GET request with text area to grab input text
+    2. Sent input text to mysql database using PHP
+    3. Displayed result back to user
+
+On the mysql side we made sure the data was prepared properly
+with the correct create.sql and load.sql script.
+
+Work Split:
+    We each did 50% as we pair programmed the entirety of the project.
--- project1b/violate.sql
+++ project1b/violate.sql
@@ -1,5 +1,29 @@
 USE CS143;
 
+/* OUTPUT FROM BELOW QUERIES 
+
+mysql> INSERT INTO MovieGenre VALUES (9090909090, 'Comedy');
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieGenre`, CONSTRAINT `MovieGenre_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`) ON DELETE CASCADE)
+mysql> INSERT INTO MovieDirector VALUES (9090909090, 1234);                             
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieDirector`, CONSTRAINT `MovieDirector_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`) ON DELETE CASCADE)
+mysql> INSERT INTO MovieDirector VALUES (1234, 9090909090);                             
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieDirector`, CONSTRAINT `MovieDirector_ibfk_2` FOREIGN KEY (`did`) REFERENCES `Director` (`id`) ON DELETE CASCADE)
+mysql> INSERT INTO MovieActor VALUES (1234, 9090909090, "James Bond");                  
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieActor`, CONSTRAINT `MovieActor_ibfk_2` FOREIGN KEY (`aid`) REFERENCES `Actor` (`id`) ON DELETE CASCADE)
+mysql> INSERT INTO MovieActor VALUES (9090909090, 1234, "James Bond");                  
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`MovieActor`, CONSTRAINT `MovieActor_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`) ON DELETE CASCADE)
+mysql> INSERT INTO Review VALUES ('asdf', '1960-07-15 23:59:59', 9090909090, 10, "James 
+Bond");                                                                                 
+ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`CS143`.`Review`, CONSTRAINT `Review_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `Movie` (`id`) ON DELETE CASCADE)
+mysql> INSERT INTO Movie VALUES (1, 'Hello', 1990, 'R', 'Max Co'), (1, 'Goodbye', 1991, 
+'PG-13', 'Ryan Co');                                                                    
+ERROR 1062 (23000): Duplicate entry '1' for key 'PRIMARY'
+mysql> INSERT INTO Actor VALUES (1, 'Chern', 'Max', 'Male', '1975-05-25', '0000-00-00'),
+ (1, 'Peterman', 'Ryan', 'Male', '1966-09-17', '0000-00-00');                           
+ERROR 1062 (23000): Duplicate entry '1' for key 'PRIMARY'
+mysql> INSERT INTO Director VALUES (1, 'Jameroni', 'Pinto', '1975-05-25', '0000-00-00'), (1, 'Abidi', 'James', '1966-09-17', '0000-00-00');                                     
+ERROR 1062 (23000): Duplicate entry '1' for key 'PRIMARY' */
+
 /* PRIMARY KEY CONSTRAINTS */
 /* ALL of these duplicates an ID among different tuples, which is not
 allowed in the primary key constraint */
